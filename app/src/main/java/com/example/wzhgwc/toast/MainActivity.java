c558a43f0f49352299b238e5634cc1efc80a48bd
gdmec07150747 <371785738@qq.com>
2016-12-16 22:45:44 +0800

    hhh

--- app/src/main/java/com/example/wzhgwc/toast/MainActivity.java
+++ app/src/main/java/com/example/wzhgwc/toast/MainActivity.java
@@ -12,6 +12,7 @@ package com.example.wzhgwc.toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
